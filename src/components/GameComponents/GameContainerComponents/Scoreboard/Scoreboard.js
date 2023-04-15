04761ec79752704b3e54a6ad1eb72a5ee20363bb
Sam Century <sam100years@hotmail.com>
2023-04-15 17:02:38 -0400

    scoreboard text

--- src/components/GameComponents/GameContainerComponents/Scoreboard/Scoreboard.js
+++ src/components/GameComponents/GameContainerComponents/Scoreboard/Scoreboard.js
@@ -1,10 +1,9 @@
 import React from 'react'
 import "./Scoreboard.css"
 import * as  TbIcons  from "react-icons/tb";
-import {motion } from 'framer-motion';
+import { motion } from 'framer-motion';
 
 const Scoreboard = ({scoreLog,state,timer}) => {
-
 
 
 
@@ -21,6 +20,19 @@ const Scoreboard = ({scoreLog,state,timer}) => {
         }        
     }
 
+    const plusVariant = {
+        active: {
+            opacity:[0,1,1,1,0],
+            color:["rgba(0,0,0,1)","rgba(255,255,255,1)","rgba(255,255,255,1)","rgba(255,255,255,1)","rgba(255,255,255,1)"],
+            transition:{
+                duration:(state.foundWords.length*0.7)-0.1
+            }
+        },
+        inactive: {
+
+        }
+    }
+
     return (
         <div className='scoreboard-layer'>
             <div className='clock-container'>
@@ -31,18 +43,38 @@ const Scoreboard = ({scoreLog,state,timer}) => {
             </div>
 
             <div className='points-container'>
+                {
+                    state.foundWords.length > 0 ? (
+                        <div className='points-display'>
+                                <motion.div 
+                                    key={`plus-${state.turn}`}
+                                    className='points-display-text'
+                                    style={{position:"absolute"}}
+                                    variants={plusVariant}
+                                    initial={{opacity:0}}
+                                    animate={"active"}
+                                >
+                                    +{state.newPoints-state.prevPoints}
+                                </motion.div>
+                
+                            <motion.div 
+                                key={`text-${state.turn}`}
+                                className='points-display-text'
+                                variants={textVariants}
+                                initial={state.foundWords.length > 0 ? {y:100, opacity:0} : {y:0,opacity:1}}
+                                animate={state.foundWords.length > 0 ? "active" : "inactive"}                        
+                            >
+                                {state.newPoints}
+                            </motion.div>
+                        </div>
+                    ) : (
+                        <div className='points-display'>
+                            <div className='points-display-text'>{state.newPoints}</div>
+                        </div>
+                    )
+                }
 
-                <div className='points-display'>
-                    <motion.div 
-                        key={`text-${state.turn}`}
-                        className='points-display-text'
-                        variants={textVariants}
-                        initial={state.foundWords.length > 0 ? {y:100, opacity:0} : {y:0,opacity:1}}
-                        animate={state.foundWords.length > 0 ? "active" : "inactive"}                        
-                    >
-                        {state.newPoints}
-                    </motion.div>
-                </div>
+
             </div>
         </div>
     )
